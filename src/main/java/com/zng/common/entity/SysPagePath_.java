5ac66f69cd82d546d63eb7aef37c377eeef9c290
John <john.zhang@facilityone.cn>
2017-12-15 18:05:12 +0800

    jersey

---
+++ src/main/java/com/zng/common/entity/SysPagePath_.java
@@ -0,0 +1,10 @@
+package com.zng.common.entity;
+
+/**
+ * Created by John.Zhang on 2017/12/15.
+ */
+public class SysPagePath_ {
+
+    public static String LOGIN = "login";
+
+}
--- src/main/java/com/zng/common/resource/PathResource.java
+++ src/main/java/com/zng/common/resource/PathResource.java
@@ -1,5 +1,7 @@
 package com.zng.common.resource;
 
+import com.zng.common.entity.SysPagePath_;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,14 +10,14 @@ import javax.ws.rs.core.MediaType;
 /**
  * Created by John.Zhang on 2017/12/14.
  */
-@Path("/rest")
+@Path("")
 public class PathResource {
 
     @GET
     @Path("say")
     @Produces(MediaType.APPLICATION_JSON)
     public String test(){
-        return "Say Hello";
+        return SysPagePath_.LOGIN;
     }
 
 }
